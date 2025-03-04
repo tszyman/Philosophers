ab3d7a549e7b23cef45dc0a6f0426fb2e874d6a4
Tomasz Szymanski <tszymans@student.42.fr>
2025-03-04 13:56:00 +0100

    first files to commit

---
+++ philo/main.c
@@ -0,0 +1,12 @@
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 5 || argc > 6)
+	{
+		printf("Error: Invalid number of arguments\n");
+		return (1);
+	}
+	printf("Program run successfully\n");
+	return (0);
+}
