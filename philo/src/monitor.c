de26c3efdeb32874dbaee9337b07c5d1a1418646
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2025-08-05 00:47:01 +0200

    meal mutex unlock corrected

--- philo/src/monitor.c
+++ philo/src/monitor.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 00:34:46 by tomek             #+#    #+#             */
+/*   Updated: 2025/08/05 00:34:47 by tomek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/philo.h"
 
 int		check_philo_death(t_philo *philo, long current_time)
@@ -7,15 +19,16 @@ int		check_philo_death(t_philo *philo, long current_time)
 
 	pthread_mutex_lock(&philo->meal_mutex);
 	last_meal = current_time - philo->last_meal_time;
-	pthread_mutex_unlock(&philo->meal_mutex);
 	is_dead = (last_meal > philo->data->time_to_die);
 	if (is_dead){
-		print_msg(philo, BLACK"died!"RESET);
+		print_msg(philo, RED"died!!!!"RESET);
 		pthread_mutex_lock(&philo->data->death_mutex);
 		philo->data->sim_end = 1;
 		pthread_mutex_unlock(&philo->data->death_mutex);
+		pthread_mutex_unlock(&philo->meal_mutex);
 		return(1);
 	}
+	pthread_mutex_unlock(&philo->meal_mutex);
 	return(0);
 }
 int		check_philos_fed(t_data *data)
@@ -50,9 +63,9 @@ void	*death_monitor(void *arg)
 		i = 0;
 		while (i < data->num_philos && !is_sim_end(data)){
 			if (!data->philos)
-			return(0);
+				return(0);
 			if (check_philo_death(&data->philos[i], curr_time))
-			return(0);
+				return(0);
 			i++;
 		}
 		if (data->must_eat_count != -1 && check_philos_fed(data)){
