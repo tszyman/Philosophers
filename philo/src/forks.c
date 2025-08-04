c23e3be5a3b0eb8bbb12474e33f787f0dfe24885
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2025-08-05 00:46:10 +0200

    last_meal update moved right after 2 fork taken; drop_forks -> similarily to taking forks for even and odd philos

--- philo/src/forks.c
+++ philo/src/forks.c
@@ -1,54 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   forks.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 00:34:24 by tomek             #+#    #+#             */
+/*   Updated: 2025/08/05 00:39:51 by tomek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/philo.h"
 
 static int	take_first_fork(t_philo *philo, pthread_mutex_t *fork)
 {
 	pthread_mutex_lock(fork);
-	if (is_sim_end(philo->data)){
+	if (is_sim_end(philo->data))
+	{
 		pthread_mutex_unlock(fork);
-		return(1);
+		return (1);
 	}
-	//print_msg(philo, "has taken first fork");
-	return(0);
-}
-static int	take_second_fork(t_philo *philo, pthread_mutex_t *first_fork, pthread_mutex_t *second_fork)
-{
-	pthread_mutex_lock(second_fork);
-	if (is_sim_end(philo->data)){
-		pthread_mutex_unlock(first_fork);
-		pthread_mutex_unlock(second_fork);
-		return(1);
-	}
-	//print_msg(philo, "has taken second fork");
-	return(0);
+	return (0);
 }
 
+static int	take_second_fork(t_philo *philo, pthread_mutex_t *first_fork,
+							pthread_mutex_t *second_fork)
+{
+	pthread_mutex_lock(second_fork);
+	if (is_sim_end(philo->data))
+	{
+		pthread_mutex_unlock(first_fork);
+		pthread_mutex_unlock(second_fork);
+		return (1);
+	}
+	pthread_mutex_lock(&philo->meal_mutex);
+	philo->last_meal_time = get_current_time();
+	pthread_mutex_unlock(&philo->meal_mutex);
+	return (0);
+}
 
 int	take_forks(t_philo *philo)
 {
 	pthread_mutex_t	*first_fork;
 	pthread_mutex_t	*second_fork;
-	// int				first_id;
-	// int				second_id;
 
-	// first_id = philo->id - 1;
-	// second_id = philo->id % philo->data->num_philos;
-	// if (first_id < second_id){
-	if (philo->id == 1){
+	if (philo->id % 2 == 0)
+	{
 		first_fork = philo->right_fork;
 		second_fork = philo->left_fork;
 	}
-	else{
+	else
+	{
 		first_fork = philo->left_fork;
 		second_fork = philo->right_fork;
 	}
 	if (take_first_fork(philo, first_fork) != 0)
-		return(1);
+		return (1);
 	if (take_second_fork(philo, first_fork, second_fork) != 0)
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
+
 void	drop_forks(t_philo *philo)
 {
-	pthread_mutex_unlock(philo->left_fork);
-	pthread_mutex_unlock(philo->right_fork);
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_unlock(philo->left_fork);
+		pthread_mutex_unlock(philo->right_fork);
+	}
+	else
+	{
+		pthread_mutex_unlock(philo->right_fork);
+		pthread_mutex_unlock(philo->left_fork);
+	}
 }
