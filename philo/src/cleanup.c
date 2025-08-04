d0f71b48d95e6bed3c34c46fe5e8a6e06926dc69
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2025-08-05 00:43:39 +0200

    norminette cleanup

--- philo/src/cleanup.c
+++ philo/src/cleanup.c
@@ -1,31 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 00:34:15 by tomek             #+#    #+#             */
+/*   Updated: 2025/08/05 00:36:31 by tomek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/philo.h"
 
 void	cleanup_data(t_data *data)
 {
 	destroy_mutexes(data);
-	if (data->philos){
+	if (data->philos)
+	{
 		free(data->philos);
 		data->philos = NULL;
 	}
-	if (data->forks){
+	if (data->forks)
+	{
 		free(data->forks);
 		data->forks = NULL;
 	}
 }
+
 void	destroy_mutexes(t_data *data)
 {
 	int	i;
 
-	if (data->philos){
+	if (data->philos)
+	{
 		i = 0;
-		while(i < data->num_philos){
+		while (i < data->num_philos)
+		{
 			pthread_mutex_destroy(&data->philos[i].meal_mutex);
 			i++;
 		}
 	}
-	if (data->forks){
+	if (data->forks)
+	{
 		i = 0;
-		while(i < data->num_philos){
+		while (i < data->num_philos)
+		{
 			pthread_mutex_destroy(&data->forks[i]);
 			i++;
 		}
